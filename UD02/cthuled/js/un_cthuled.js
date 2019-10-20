b8b5b0c06832910f998c42b4cdac611c55372c98
Jgarciaortega <jagaroc@outlook.es>
2019-10-20 16:24:29 +0200

    asignacion aleatoria de elementos sobre el tablero hecha

--- UD02/cthuled/js/un_cthuled.js
+++ UD02/cthuled/js/un_cthuled.js
@@ -20,8 +20,7 @@ var mapa = [
 //Columna descubierta = true cubierta = false;
 var estadoColumnas = [];
 
-//Elementos que deben contener ocultas las columnas
-var elementos = ["Llave", "Pergamino", "Villano", "Esmeralda"];
+
 
 //BOOLEAN PARA ABRIR PUERTA SALIDA
 var salida = true;
@@ -102,7 +101,7 @@ function recogerPulsacion(event) {
     if (event == "ArrowRight") {
 
 
-        if (x < 20 && mapa[y][x + 1] != 1) {
+        if (y > 0 && x < 20 && mapa[y][x + 1] != 1) {
 
             posJugador.classList.remove("sonic" + direccion);
             posJugador.classList.add("caminoPisado");
@@ -117,7 +116,7 @@ function recogerPulsacion(event) {
 
     if (event == "ArrowLeft") {
 
-        if (x > 0 && mapa[y][x - 1] != 1) {
+        if (y > 0 && x > 0 && mapa[y][x - 1] != 1) {
 
             posJugador.classList.remove("sonic" + direccion);
             posJugador.classList.add("caminoPisado");
@@ -134,8 +133,6 @@ function recogerPulsacion(event) {
     //console.table(mapa);
     comprobarColumnas();
 
-    //Marcamos a cada paso las columnas que rodeamos marcando su valor en la matriz
-    // marcarCamino(y, x);
 
 }
 
@@ -154,10 +151,10 @@ function comprobarColumnas() {
             estadoColumnas.push(completa);
 
         }
-       
+
     }
 
-    console.table(estadoColumnas);
+    revisarEstilos(estadoColumnas);
     estadoColumnas = [];
 
 }
@@ -189,7 +186,7 @@ function rodearColumna(PosY, PosX) {
 
             cont++;
         }
-       
+
 
     }
 
@@ -227,60 +224,146 @@ function rodearColumna(PosY, PosX) {
     return completa;
 }
 
+
+function revisarEstilos(estadoColumnas) {
+
+    let columnasModificadas = [];
+
+    for (let i = 0; i < estadoColumnas.length; i++) {
+
+        if (estadoColumnas[i] == true) {
+
+            columnasModificadas = document.getElementsByClassName("columna" + i);
+            modificarEstilo(columnasModificadas);
+
+        }
+    }
+
+}
+
+function modificarEstilo(columnasModificadas) {
+
+    for (let i = 0; i < columnasModificadas.length; i++) {
+
+        columnasModificadas[i].classList.remove("columna");
+
+    }
+
+}
+
 function crearMapa() {
 
     let numColumna = 0;
+    let memoColumna = 0;
     let cont1 = 0;
+    let cont2 = 0;
     let contY = 2;
 
-    for (let i = 0; i < mapa.length; i++) {
+    for (let y = 0; y < mapa.length; y++) {
 
-        for (let j = 0; j < mapa[0].length; j++) {
+        for (let x = 0; x < mapa[0].length; x++) {
 
             let newDiv = document.createElement("div");
-            newDiv.setAttribute("id", i + " " + j);
+            newDiv.setAttribute("id", y + " " + x);
             let elemento = "";
-      
-            if (i != 0) newDiv.classList.add("camino");
-    
-            if (mapa[i][j] == 1) {  
-                          
+
+            /* RELLENA CAMINO */
+            if (y != 0) newDiv.classList.add("camino");
+
+            /* RELLENA COLUMNAS Y ASIGNA SUS POSIBLES CLASES */
+            if (mapa[y][x] == 1 && contY == y) {
+
                 newDiv.classList.add("columna");
                 newDiv.classList.add("columna" + numColumna);
+
                 cont1++;
 
+                if (cont1 == 3) {
+
+                    numColumna++;
+                    cont1 = 0;
+
+                }
+
+                if (x == 19) {
+
+                    if (cont2 == 0) {
+
+                        contY++;
+                        cont2++;
+                        numColumna = memoColumna;
+
+                    } else {
+
+                        cont2 = 0;
+                        contY += 2;
+                        memoColumna = numColumna;
+
+                    }
+
+                }
+
             }
-    
-            if (mapa[i][j] == 2) newDiv.classList.add("sonicStatic");
-        
-            if (mapa[i][j] == 3) newDiv.classList.add("eggmanStatic");
-    
-    
+
+            if (mapa[y][x] == 2) newDiv.classList.add("sonicStatic");
+
+            if (mapa[y][x] == 3) newDiv.classList.add("eggmanStatic");
+
+
             document.getElementById("mapa").appendChild(newDiv);
-    
-        }
-    
+
         }
 
     }
 
+    asignarElementosEnTablero();
 
-function elegirElemento() {
+}
 
-    let aleatorio = Math.round(Math.random() * elementos.length);
+function asignarElementosEnTablero() {
+
+    //Elementos que deben contener ocultas las columnas
+    let elementos = ["Llave", "Pergamino", "Villano", "Esmeralda", "Nada", "Nada", "Nada", "Nada", "Nada"
+        , "Nada", "Nada", "Nada", "Nada", "Nada", "Nada", "Nada", "Nada", "Nada", "Nada", "Nada"];
+
     let elemento = "";
 
-    elemento = elementos[aleatorio];
-    elementos.splice(aleatorio, 1);
+    let serieAleatoria = generarSerieAleatoria();
 
-    return elemento;
+    for (let i = 0; i < 20; i++) {
 
-     // if (PosJ % 2 == 0 && PosI % 2 != 0) {
+        let divsColumnas = document.getElementsByClassName("columna" + serieAleatoria[i]);
+        elegirElemento(divsColumnas, elementos[i]);
 
-            //     elemento = elegirElemento();
 
-            //     newDiv.classList.add("columna" + elemento);
+    }
+}
 
-            // }
+function generarSerieAleatoria() {
+
+    var listaNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+
+    var i, j, k;
+
+    for (i = listaNum.length; i > 0; i--) {
+
+        j = Math.floor(Math.random() * i);
+        k = listaNum[i - 1];
+
+        listaNum[i - 1] = listaNum[j];
+        listaNum[j] = k;
+
+    }
+
+    return listaNum;
+
+}
+
+function elegirElemento(divsColumnas, elemento) {
+
+    for (let i = 0; i < divsColumnas.length; i++) {
+
+        divsColumnas[i].classList.add("columna" + elemento);
+    }
 
 }
