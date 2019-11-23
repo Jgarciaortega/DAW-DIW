53513befbd11ceab7ea1d8f69255b6714aeffbc1
Jgarciaortega <jagaroc@outlook.es>
2019-11-23 13:43:47 +0100

    todas la funcionalidades ok. faltan retoques esteticos

--- UD02/Migracion/js/migracion.js
+++ UD02/Migracion/js/migracion.js
@@ -1,19 +1,39 @@
-/*
+// This Script is licensed under GPL v3 or higher
 
-This Script is licensed under GPL v3 or higher
+// Author: Angel Berlanas Vicente
+// email : <berlanas_ang@gva.es>
 
-Author: Angel Berlanas Vicente
-email : <berlanas_ang@gva.es>
 
-*/
+function sleep(ms) {
 
-/*
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-FUNCIONES PERDIDAS
-^(;,;)^
+async function avanceBarra(step,dataStep) {
 
-*/
+    let steps = document.querySelectorAll('[data-step]');
+    let cont = 0;
+    dataStep++;
+   
+    for (let i = 0; i < 101; i++) {
+        
+        if (i == 10 + cont) {
+            
+            await sleep(200);
+            cont += 10;
+        }
 
+        step.setAttribute('value', +i);
+    }
+    //Tras finalizar el incremento de la barra simulamos click para mostrar finalsmg
+    if (dataStep < steps.length + 1)   document.querySelector(`[data-step="${dataStep}"]`).click();
+   
+}
+
+function añadirEstabaEscondido(){
+
+    this.classList.add('estabaEscondido');
+}
 
 function transicionar(ev) {
 
@@ -21,13 +41,16 @@ function transicionar(ev) {
     let steps = document.querySelectorAll('[data-step]');
     let step = document.querySelector(`[data-step="${dataStep}"]`);
 
-    if (dataStep < steps.length+1) {
+    if (dataStep < steps.length + 1) {
 
         step.addEventListener('transitionend', transicionar);
-        step.classList.add('estabaEscondido');
-      
-        // if (step.localName == 'progress') 
-        // if (step.localName == 'finalmsg') 
+        
+        /*Los pasos tendran un evento transitioned, excepto el finalmsg, que tendra una simulacion de click cuando
+        finalice el paso anterior*/
+
+        if (step.localName == 'steplabel') step.classList.add('estabaEscondido');
+        if (step.localName == 'progress') step.classList.add('estabaEscondido'); avanceBarra(step,dataStep);
+        if (step.localName == 'finalmsg') step.addEventListener('click', añadirEstabaEscondido);
 
     }
 }
@@ -44,7 +67,6 @@ function init() {
 
     // Set click function on button
     document.querySelector("button").addEventListener("click", startMigration);
-
 
 }
 
