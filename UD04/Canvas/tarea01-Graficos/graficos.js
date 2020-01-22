89690b36f31425c011a18f25e2e6df9a219bc1d3
Jgarciaortega <jagaroc@outlook.es>
2020-01-22 01:20:25 +0100

    grafico de puntos y ultimos retoques

--- UD04/Canvas/tarea01-Graficos/graficos.js
+++ UD04/Canvas/tarea01-Graficos/graficos.js
@@ -5,6 +5,9 @@ function buildGrafico() {
     let ctx = canvas.getContext('2d');
     let type = document.getElementById('selectGraphic');
     let selectedType = type.options[type.selectedIndex].text;
+    let padding = 20;
+    let canvasActualWidth = canvas.width - padding * 2;
+    let canvasActualHeight = canvas.height - padding * 2;
     //variables circle graphic:
     let circleGraphic = false;
     let total;
@@ -13,165 +16,156 @@ function buildGrafico() {
     //variables bar graphic:
     let barGraphic = false;
     let maxNum;
-
-    //Init canvas:
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-
-    //Init legend
-    document.querySelector('legend[for="myCanvas"]');
-
     //variables point graphic:
     let pointGraphic = false;
+    let startX = 0;
+    let startY = canvasActualHeight + padding;
+    let endX = 0;
+    let endY = 0;
 
-    if(selectedType == 'Grafico tarta') circleGraphic = true;
+    //Init canvas:
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    //Init legend
+    document.querySelector('legend[for="myCanvas"]').innerHTML = '';
+
+    if (selectedType == 'Grafico tarta') circleGraphic = true;
     else if (selectedType == 'Grafico barras') barGraphic = true;
     else if (selectedType == 'Grafico puntos') pointGraphic = true;
 
-    if(barGraphic || pointGraphic){
-         //averigua el mayor valor de los datos recogidos
-         maxNum = getMaxNum();
-         paintGrid(canvas, ctx, maxNum);
+     //averigua el mayor valor de los datos recogidos
+     maxNum = getMaxNum();
 
-    } 
+    if (barGraphic || pointGraphic) {
+       
+        paintGrid(canvas, ctx, maxNum);
+        createLegend();
+    }
+
     for (let i = 1; i < 5; i++) {
 
-       let clave = document.querySelector("input[name='key_" + i + "']").value;
-       let valor = document.querySelector("input[name='value_" + i + "']").value;
+        let clave = document.querySelector("input[name='key_" + i + "']").value;
+        let valor = document.querySelector("input[name='value_" + i + "']").value;
 
         if (clave != '' && valor != '') {
 
             if (circleGraphic) {
-                //obtenemos el total de todos los valores
+
                 total = getValorTotal();
-               
+
                 angulo = 2 * Math.PI * valor / total;
-               
+
                 buildCircleGraphic(canvas, ctx, clave, angulo, inicioAngulo, colores[i]);
 
                 inicioAngulo += angulo;
 
             } else if (barGraphic) {
 
-                buildBarGraphic(canvas, ctx, maxNum, valor, colores[i], i, colores2[i]) ;
-               
-            } else if (pointGraphic){
+                buildBarGraphic(canvas, ctx, maxNum, valor, colores[i], i, colores2[i], padding, canvasActualWidth,  canvasActualHeight);
 
-                //buildPointGraphic(canvas, ctx, maxNum, valor, colores[i], i);
+            } else if (pointGraphic) {
+
+                valor = parseInt(valor); 
+                endX  += canvas.width / 4 - 2;        
+                endY  = canvas.height - (Math.round(canvasActualHeight * valor  / maxNum) + padding);
+
+                buildPointGraphic(ctx, startX, startY, endX, endY, colores[i]);  
+                
+                startX = endX;
+                startY = endY;
             }
-
+            
         }
     }
 
-    createLegend();
-
+   
 }
 
+function buildPointGraphic(ctx, startX ,startY,endX, endY, color) {
 
-function buildPointGraphic(canvas, ctx, maxNum, valor, color, index){
-
-     //distancia en pixeles del borde del canvas hasta la gráfica
-     let padding = 20;
-     let canvasActualHeight = canvas.height - padding * 2;
-     //valor inicial de la escala
-     let gridValue = 0;
-     //incremento de los valores de la escala
-     let gridScale = 100;
-     let gridY;
- 
-     while(gridValue <= maxNum){
- 
-         gridY = canvasActualHeight * (1 - gridValue/maxNum) + padding;
-         drawLine(ctx,0,gridY,canvas.width,gridY);
-         ctx.save();
-         ctx.fillStyle = "#4C4B49";
-         ctx.font = "bold 15pt Calibri";
-         ctx.fillText(gridValue, 10, gridY -2);
-         ctx.restore();
-         gridValue += gridScale;
- 
-     }
-
+    drawLine(ctx,startX,startY, endX, endY, color);
+    
+    ctx.beginPath();
+    ctx.moveTo(startX, startY);
+    ctx.arc(endX, endY, 5, 0, 2*Math.PI);
+    ctx.closePath();
+    ctx.fill();
 }
 
-function createLegend(){
+function createLegend() {
 
     let legend = document.querySelector("legend[for='myCanvas']");
     let ul = document.createElement('ul');
-    ul.setAttribute('id','legend');
+    ul.setAttribute('id', 'legend');
     legend.append(ul);
     let clave;
-    
+
     for (let i = 1; i < 5; i++) {
-        
+
         clave = document.querySelector("input[name='key_" + i + "']").value;
-        let li = document.createElement('li');  
+        let li = document.createElement('li');
         li.style.borderLeft = "20px solid " + colores[i];
         li.textContent = clave;
         ul.appendChild(li);
-        
+
     }
 
 }
 
 /* PINTA ESCALA GRAFICA */
-function paintGrid(canvas, ctx, maxNum, valor, color, index){
+function paintGrid(canvas, ctx, maxNum) {
 
-    console.log('paintingGrid');
-    //distancia en pixeles del borde del canvas hasta la gráfica
     let padding = 20;
     let canvasActualHeight = canvas.height - padding * 2;
     //valor inicial de la escala
     let gridValue = 0;
     //incremento de los valores de la escala
-    let gridScale = 100;
+    let gridScale = Math.round (maxNum / 5);
     let gridY;
 
-    while(gridValue <= maxNum){
+    while (gridValue <= maxNum) {
 
-        gridY = canvasActualHeight * (1 - gridValue/maxNum) + padding;
-        drawLine(ctx,0,gridY,canvas.width,gridY);
+        gridY = canvasActualHeight * (1 - gridValue / maxNum) + padding;
+        drawLine(ctx, 0, gridY, canvas.width, gridY, "#4C4B49");
         ctx.save();
         ctx.fillStyle = "#4C4B49";
         ctx.font = "bold 15pt Calibri";
-        ctx.fillText(gridValue, 10, gridY -2);
+        ctx.fillText(gridValue, 10, gridY - 2);
         ctx.restore();
         gridValue += gridScale;
 
     }
 }
 
-function buildBarGraphic(canvas, ctx, maxNum, valor, color, index,color2) {
+function buildBarGraphic(canvas, ctx, maxNum, valor, color, index, color2, padding, canvasActualWidth,  canvasActualHeight ) {
 
-    let padding = 20;
-    let canvasActualWidth  = canvas.width  - padding * 2; 
-    let canvasActualHeight = canvas.height - padding * 2; 
     let barIndex = index - 1;
     let numberOfBars = 4;
-    let barHeight = Math.round(canvasActualHeight * valor/maxNum);
-    let barSize = (canvasActualWidth) / numberOfBars;
-    
-    drawBar(ctx, (padding + barIndex  * barSize) + 5, (canvas.height - barHeight - padding) - 5, barSize, barHeight, color2);
-    drawBar(ctx, (padding + barIndex  * barSize), (canvas.height - barHeight - padding), barSize, barHeight, color);
+    let barHeight = Math.round(canvasActualHeight * valor / maxNum);
+    let barSize = (canvasActualWidth-28) / numberOfBars;
+
+    drawBar(ctx, (padding + barIndex * barSize) + 40, (canvas.height - barHeight - padding) - 5, barSize, barHeight, color2);
+    drawBar(ctx, (padding + barIndex * barSize) + 35, (canvas.height - barHeight - padding), barSize, barHeight, color);
 
 
 }
 
-/* PINTA LA ESCALA DE LA GRAFICA */
-function drawLine(ctx, startX, startY, endX, endY){
+function drawLine(ctx, startX, startY, endX, endY, color) {
+
     ctx.save();
-    ctx.strokeStyle = "#4C4B49";
+    ctx.strokeStyle = color;
     ctx.beginPath();
-    ctx.moveTo(startX,startY);
-    ctx.lineTo(endX,endY);
+    ctx.moveTo(startX, startY);
+    ctx.lineTo(endX, endY);
     ctx.stroke();
     ctx.restore();
 }
 
 /* PINTA LAS BARRAS */
-function drawBar(ctx, upperLeftCornerX, upperLeftCornerY, width, height,color){
+function drawBar(ctx, upperLeftCornerX, upperLeftCornerY, width, height, color) {
     ctx.save();
-    ctx.fillStyle=color;
-    ctx.fillRect(upperLeftCornerX,upperLeftCornerY,width,height);
+    ctx.fillStyle = color;
+    ctx.fillRect(upperLeftCornerX, upperLeftCornerY, width, height);
     ctx.restore();
 }
 
@@ -213,21 +207,21 @@ function loadListeners() {
     document.querySelector("input[name='grafiqueame']").addEventListener("click", buildGrafico);
 }
 
-function getMaxNum(){
+function getMaxNum() {
 
     let maxNum = -1;
 
     for (let i = 1; i < 5; i++) {
-        
+
         clave = document.querySelector("input[name='key_" + i + "']");
         valor = document.querySelector("input[name='value_" + i + "']");
 
         if (clave.value != '' && valor.value != '') {
 
-            if(valor.value > maxNum) maxNum = valor.value;
+            if (valor.value > maxNum) maxNum = valor.value;
 
         }
-        
+
     }
     return maxNum;
 }
@@ -258,7 +252,7 @@ function init() {
 
 }
 
-const colores2 = ['','darkred','darkblue','darkgreen','orange'];
+const colores2 = ['', 'darkred', 'darkblue', 'darkgreen', 'orange'];
 const colores = ['', 'red', 'blue', 'green', 'yellow'];
 
 window.onload = init;
